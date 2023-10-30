4de7741f41620c6278b03e576f958810278ba9f2
juliayemin <juliamin@MacBook-Pro.local>
2023-10-29 21:04:52 -0700

    upload

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "Y6a69a4ecf55e3b1486db843ac6a5daaf"
+weather_api_key = "e64704e86c6493415f9118898f24cc72"
 
 # Geoapify API Key
 geoapify_key = "db0fe88062ef4e1a8716c46f36de8fb2"
